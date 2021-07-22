0aedfcdf38f96050517a34bae8ca3933d913bad3
AJ-Romera <ajromeraalfonso@gmail.com>
2021-07-22 17:34:38 +0200

    CHNG: Make & Render Attribution Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Attribution from './components/Attribution';
 import './index.css';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<Attribution />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Attribution.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Attribution() {
+	return (
+		<div class='attribution'>
+			Challenge by{' '}
+			<a
+				href='https://www.frontendmentor.io/challenges/job-listings-with-filtering-ivstIPCt'
+				rel='noreferrer'
+				target='_blank'
+			>
+				Frontend Mentor
+			</a>
+			. Coded by{' '}
+			<a href='https://www.linkedin.com/in/aj-romera/'>AJ-Romera</a>.
+		</div>
+	);
+}
+
+export default Attribution;
