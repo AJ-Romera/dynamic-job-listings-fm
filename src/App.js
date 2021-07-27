036e79d955b7ec511c78d6bbba8bb924247eb3a7
AJ-Romera <ajromeraalfonso@gmail.com>
2021-07-27 17:26:09 +0200

    CHNG: Style the FilterTable

--- src/App.js
+++ src/App.js
@@ -9,21 +9,79 @@ import Header from './components/Header';
 const Container = styled.main`
 	width: auto;
 	margin: 3.438rem auto 0 auto;
-	margin-top: 3.438rem;
 	padding: 0 1.438rem;
 `;
 
-/* const Card = styled.div`
+const FilterTable = styled.div`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
 	width: 100%;
-	height: auto;
-	position: relative;
-	margin-bottom: 2.375rem;
-	padding: 2rem 1.5rem 1.5rem 1.5rem;
+	margin-top: -5.75rem;
+	margin-bottom: 3.5rem;
+	padding: 0.8rem 1.188rem;
 	border-radius: 0.425rem;
 	background-color: #ffffff;
 	-webkit-box-shadow: 3px 12px 26px -22px var(--desaturated-dark-cyan);
 	box-shadow: 3px 12px 26px -22px var(--desaturated-dark-cyan);
-`; */
+`;
+
+const TableLeft = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+	width: 80%;
+`;
+
+const Keyword = styled.span`
+	position: relative;
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+	background-color: var(--light-grayish-cyan-filter-tablets);
+	margin: 0.5rem 3rem 0.5rem 0rem;
+	padding: 0.6rem 0.5rem 0.4rem 0.5rem;
+	border-radius: 0.3rem 0 0 0.3rem;
+	font-size: 0.79rem;
+	color: var(--desaturated-dark-cyan);
+	text-transform: capitalize;
+	font-weight: 700;
+
+	&:after {
+		position: absolute;
+		right: -2.1rem;
+		top: 0;
+		bottom: 0;
+		content: '';
+		padding: 0.7rem 1rem;
+		border-radius: 0 0.3rem 0.3rem 0;
+		background-color: var(--desaturated-dark-cyan);
+		background-image: url(images/icon-remove.svg);
+		background-repeat: no-repeat;
+		background-position: center;
+		/* background-size: auto; */
+	}
+
+	&:hover:after {
+		cursor: pointer;
+		background-color: var(--very-dark-grayish-cyan);
+	}
+`;
+
+const TableRight = styled.div`
+	display: flex;
+	width: 20%;
+	justify-content: center;
+	align-items: center;
+	font-size: 0.79rem;
+	font-weight: 700;
+	color: var(--dark-grayish-cyan);
+
+	&:hover {
+		cursor: pointer;
+		text-decoration: underline;
+	}
+`;
 
 function App() {
 	const [jobs, setJobs] = useState([]);
@@ -57,9 +115,15 @@ function App() {
 		<div className='App'>
 			<Header />
 			<Container>
-				<div>
-					{filter.length > 0 && filter.map((fi) => <span>{fi}</span>)}
-				</div>
+				<FilterTable>
+					<TableLeft>
+						{filter.length > 0 &&
+							filter.map((filteredKeyword) => (
+								<Keyword>{filteredKeyword}</Keyword>
+							))}
+					</TableLeft>
+					<TableRight>Clear</TableRight>
+				</FilterTable>
 				{jobs.length === 0 ? (
 					<p>No jobs found...</p>
 				) : (
